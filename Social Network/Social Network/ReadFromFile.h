8947c081e304fbeba89e1c20bc2b2717184d4b4e
ha55anali <45466399+ha55anali@users.noreply.github.com>
2018-11-30 15:34:06 +0500

    File input v2 with comments

--- Social Network/Social Network/ReadFromFile.h
+++ Social Network/Social Network/ReadFromFile.h
@@ -4,32 +4,39 @@
 using namespace std;
 
 #define FilePath "input.txt"
+#define FileLineLenght 1000
 
 int * NumBuffToArr(char * LineOfText, int NumCount) {
-	int *ArrOfNum = new int[NumCount + 1];
-	char NumBuff[10000];
+	int *ArrOfNum = new int[NumCount + 1]; //array to store numbers
+	char NumBuff[FileLineLenght]; //char array to store each number before conversion to int
 	int NumBuffCount = 0;
-	int LotCount = 0;
+	int ArrOfNumCount = 0;
 
 	for (int c = 0; LineOfText[c] != '\0'; ++c) {
 		if (LineOfText[c] == ',') {
+			//previous number has ended
 			NumBuffCount = 0;
-			ArrOfNum[LotCount] = atoi(NumBuff);
-			++LotCount;
+			ArrOfNum[ArrOfNumCount] = atoi(NumBuff);
+			++ArrOfNumCount;
 		}
 		else {
+			//append to previous number
 			NumBuff[NumBuffCount] = LineOfText[c];
 			NumBuff[NumBuffCount + 1] = '\0';
 			++NumBuffCount;
 		}
 	}
-	ArrOfNum[LotCount] = atoi(NumBuff);
-	++LotCount;
-	ArrOfNum[LotCount] = -1;
+	//store the number in NumBuff to ArrOfNum
+	ArrOfNum[ArrOfNumCount] = atoi(NumBuff);
+	++ArrOfNumCount;
+	//add sentinal
+	ArrOfNum[ArrOfNumCount] = -1;
+
 	return ArrOfNum;
 }
 
-int * CreateFriendArr(char LineOfText[]) {
+int * CreateNumArr(char LineOfText[]) {
+	//counts amount of integers in LineOfText
 	int NumCount = 0;
 	for (int c = 0; LineOfText[c] != '\0'; ++c) {
 		if (LineOfText[c] == ',') {
@@ -38,34 +45,38 @@ int * CreateFriendArr(char LineOfText[]) {
 	}
 	++NumCount;
 
+	//function returns int pointer array with numbers
 	return NumBuffToArr(LineOfText, NumCount);
 }
 
 int ** ReadFromFile(int &FriendNum) {
 	ifstream file(FilePath);
 
+	//if file doesnt open
 	if (file.fail()) {
 		return nullptr;
 	}
 
 	int LineCount = 0;
-	char LineOfText[1000];
+	char LineOfText[FileLineLenght];
 
+	//read to end of file to count number of lines
 	while (!file.eof()) {
-		file.getline(LineOfText, 1000);
+		file.getline(LineOfText, FileLineLenght);
 		++LineCount;
 	}
-
+	
+	//returns file pointer to start
 	file.clear();
 	file.seekg(0, ios::beg);
 
 	int **spine = new int *[LineCount];
-
 	int count = 0;
 
 	while (!file.eof()) {
-		file.getline(LineOfText, 1000);
-		spine[count] = CreateFriendArr(LineOfText);
+		file.getline(LineOfText, FileLineLenght);
+		//function returns int array pointer with the numbers 
+		spine[count] = CreateNumArr(LineOfText);
 		++count;
 	}
 
