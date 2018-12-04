2b4dc5d91c8d540bebd7347f7c897a5af5c1b414
ha55anali <45466399+ha55anali@users.noreply.github.com>
2018-12-04 21:36:35 +0500

    print board

---
+++ Chess/Chess/Print Board.h
@@ -0,0 +1,67 @@
+#pragma once
+
+void PrintRow(char Board[][BoardLenght], int RowNum, int cellheight, int cellwidth) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	bool Flag; //checks whether the row has white first or black cell first
+	if (RowNum % 2 == 0)
+		Flag = 0; //black first
+	else
+		Flag = 1; //white first
+
+	for (int x = 0; x < cellheight; ++x) {
+
+		for (int col = 0; col < BoardLenght; ++col) {
+
+			//set color of the cell
+			if (col % 2 == Flag) {
+				SetConsoleTextAttribute(hConsole, 240);
+			}
+			else {
+				SetConsoleTextAttribute(hConsole, 15);
+			}
+
+			//first the cell
+			for (int c = 0; c < cellwidth; ++c) {
+				//checks if the peice has to be printed
+				if (x + 1 == cellheight / 2 && c + 1 == cellwidth / 2) {
+					cout << Board[RowNum][col];
+				}
+				else {
+					cout << " ";
+				}
+			}
+		}
+
+		//outputs labels
+		SetConsoleTextAttribute(hConsole, 15);
+		if (x + 1 == cellheight / 2) {
+			cout << "  " << RowNum;
+		}
+
+		cout << endl;
+	}
+}
+
+void PrintBoard(char Board[][BoardLenght]) {
+
+	int cellwidth = 8;
+	int cellheight = cellwidth / 2;
+
+	//prints alphabetical labels
+	for (int x = 0; x < BoardLenght; ++x) {
+		for (int c = 0; c + 1 < cellwidth / 2; ++c) {
+			cout << " ";
+		}
+		cout << (char)('A' + x);
+		for (int c = 0; c < cellwidth / 2; ++c) {
+			cout << " ";
+		}
+	}
+	cout << endl << endl;
+
+
+	for (int row = 0; row < BoardLenght; ++row) {
+		PrintRow(Board, row, cellheight, cellwidth);
+	}
+}
--- Chess/Chess/Source.cpp
+++ Chess/Chess/Source.cpp
@@ -1,85 +1,16 @@
 #include <iostream>
+#include <Windows.h>
 
-
+#include "init Board.h"
+#include "Print Board.h"
 
 using namespace std;
 
-#define BoardLenght 8
-#define white 0
-#define black 1
 
-void initLastRow(char Board[][BoardLenght], bool Player) {
-	int Temp;
-	int row;
 
-	if (Player == white) {
-		row = 0;
-		Temp = 'A' - 'a';
-	}
-	else {
-		row = 7;
-		Temp = 0;
-	}
-
-	Board[row][0] = 'r' + Temp;
-	Board[row][7] = 'r' + Temp;
-
-	Board[row][1] = 'n' + Temp;
-	Board[row][6] = 'n' + Temp;
-
-	Board[row][2] = 'b' + Temp;
-	Board[row][5] = 'b' + Temp;
-
-	Board[row][3] = 'q' + Temp;
-	Board[row][4] = 'k' + Temp;
-}
-
-void initPawnRow(char Board[][BoardLenght],int Player) {
-	int row;
-	int Temp;
-
-	if (Player == white) {
-		row = 1;
-		Temp = 'A' - 'a';
-	}
-	else {
-		row = 6;
-		Temp = 0;
-	}
-
-	for (int col = 0; col < BoardLenght; ++col) {
-		Board[row][col]= 'p' + Temp;
-	}
-}
-
-void initEmptySpaces(char Board[][BoardLenght]) {
-	for (int row = 2; row < BoardLenght-2; ++row) {
-		for (int col = 0; col < BoardLenght; ++col) {
-			Board[row][col] = ' ';
-		}
-	}
-}
-
-void initBoard(char Board[][BoardLenght]) {
-	initLastRow(Board, white);
-	initLastRow(Board, black);
-
-	initPawnRow(Board, white);
-	initPawnRow(Board, black);
-
-	initEmptySpaces(Board);
-}
-
-void PrintBoard(char Board[][BoardLenght]) {
-	for (int c = 0; c < BoardLenght; ++c) {
-		for (int x = 0; x < BoardLenght; ++x) {
-			cout << Board[c][x] << " ";
-		}
-		cout << endl;
-	}
-}
 
 int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	char Board[BoardLenght][BoardLenght];
 
